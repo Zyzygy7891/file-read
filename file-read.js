ca4cfa41844b830656182aa47f43eccfa40cfb21
Zyzygy7891 <suranyiattila87@gmail.com>
2023-05-27 15:52:31 +0200

    remove sample url

--- file-read.js
+++ file-read.js
@@ -1,27 +1,40 @@
 
-const remotefile = new URL('https://www.budavartours.hu/binaries/content/assets/budavar/sitemap-bvt.xml'); // A letölteni kívánt fájl URL-je
+const remotefile = new URL(''); // A letölteni kívánt fájl URL-je
 
-const connection = require(remotefile.protocol.replace(':','')); // Az URL protokollja alapján létrehozott http vagy https
+const connection = require(remotefile.protocol.replace(':', '')); // Az URL protokollja alapján létrehozott http vagy https
+const updateInterval = 500; // Frissítési periódus (másodperc)
 
 function downloadFile(url) {
   return new Promise((resolve, reject) => {
     connection.get(url, (response) => {
+      const logFileProgress = (downloadedBytes, contentLength, isAllMessageInOneLine=true)=>
+      {
+        process.stdout.write(`Letöltve: ${~~(100 * (downloadedBytes / contentLength))}% ${downloadedBytes} bájt / ${contentLength} bájt${isAllMessageInOneLine?'\r':'\n'}`);
+      }
+
       const contentLength = response.headers['content-length']; // A válaszban kapott fájl mérete
 
       const dataChunks = []; // Buffer tömb a letöltött adatok tárolásához
       let downloadedBytes = 0; // Letöltött bájtok száma
+      let lastUpdate = Date.now(); // Utolsó frissítés időpontja
 
       response.on('data', (chunk) => {
         dataChunks.push(chunk); // Letöltött adat hozzáadása a Buffer tömbhöz
         downloadedBytes += chunk.length; // Letöltött adat méretének frissítése
+        const currentTime = Date.now();
+        const elapsedTime = currentTime - lastUpdate;
 
-        process.stdout.write(`Letöltve: ${(100*(downloadedBytes/contentLength)).toFixed(1)}% ${downloadedBytes} bájt / ${contentLength} bájt\r`); // Kiírás a konzolra egy sorba
+        if (elapsedTime >= updateInterval) {
+          logFileProgress(downloadedBytes, contentLength); // Fájl készültségének és méretének kiírása egyetlen sorba
+          lastUpdate = currentTime; // Frissítési időpont frissítése
+        }
       });
 
       response.on('end', () => {
         const downloadedData = Buffer.concat(dataChunks); // Letöltött adatok összefűzése egyetlen Buffer-be
 
-        console.log('\nLetöltés befejeződött!');
+        logFileProgress(downloadedBytes, contentLength, false); // Fájl készültségének és méretének kiírása egyetlen sorba
+        console.log('Letöltés befejeződött!');
         resolve(downloadedData); // Promise teljesítése a letöltött adattal
       });
     }).on('error', (error) => {
